d83513f537dfffb4f82b3537d63ecb06cbac9742
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2023-11-21 18:57:50 +0900

    feat(TriangularSnail): Add TriangularSnail and init

---
+++ src/chapter3/array/triangularsnail/TriangularSnail.java
@@ -0,0 +1,18 @@
+package chapter3.array.triangularsnail;
+
+public class TriangularSnail {
+
+    /*
+     * 정수 n이 매개변수로 주어집니다.
+     * 다음 그림과 같이 밑변의 길이와 높이가 n인 삼각형에서 맨 위 꼭짓점부터 반시계 방향으로 달팽이 채우기를 진행한 후,
+     * 첫 행부터 마지막 행까지 모두 순서대로 합친 새로운 배열을 반환하라.
+     */
+    public int[] existingCode(int n) {
+        int[] answer = {};
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        TriangularSnail snail = new TriangularSnail();
+    }
+}
