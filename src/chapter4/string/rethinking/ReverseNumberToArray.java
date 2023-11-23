938daa14fe0155977b41a96fb029720b84268226
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2023-11-23 15:06:52 +0900

    feat(ReverseNumberToArray): Convert each character in array to integer

--- src/chapter4/string/rethinking/ReverseNumberToArray.java
+++ src/chapter4/string/rethinking/ReverseNumberToArray.java
@@ -1,5 +1,7 @@
 package chapter4.string.rethinking;
 
+import java.util.Arrays;
+
 public class ReverseNumberToArray {
 
     /**
@@ -15,7 +17,7 @@ public class ReverseNumberToArray {
      * 3. 뒤집힌 문자열을 문자의 배열로 변환한다.
      * 4. 배열의 각 문자를 정수로 변환한다.
      */
-    public int[] solution_arrayList(long n) {
+    public int[] solution(long n) {
         // 입력받은 숫자를 문자열로 변환한다.
         // 이는 정수 long형의 wrapper class인 Long의 static 메서드인 toString()을 사용한다.
         String str = Long.toString(n);
@@ -23,10 +25,17 @@ public class ReverseNumberToArray {
         String reversed = new StringBuilder(str).reverse().toString();
         // 뒤집힌 문자열은 String 클래스의 toCharArray() 메서드를 사용하여 문자 배열로 변환할 수 있다.
         char[] arr = reversed.toCharArray();
+
+        // 마지막으로 이를 정수로 변환하여 int형 배열을 만들어 줄 수 있다.
+        int[] result = new int[arr.length];
+        for (int i = 0; i < result.length; i++) {
+            result[i] = arr[i] - '0';
+        }
+        return result;
     }
 
     public static void main(String[] args) {
         ReverseNumberToArray array = new ReverseNumberToArray();
-        System.out.println("Result: " + array);
+        System.out.println("Result: " + Arrays.toString(array.solution(12345)));
     }
 }
